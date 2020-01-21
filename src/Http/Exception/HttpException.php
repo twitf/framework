ecc6ef9dd0770ca629d3ebfd56321bd434fcb855
twitf <twitf@twitfdeHackintosh.local>
2020-01-21 23:42:49 +0800

    增加缺失文件

---
+++ src/Http/Exception/HttpException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Twitf\Framework\Http\Exception;
+
+class HttpException extends \RuntimeException implements HttpExceptionInterface
+{
+    private $statusCode;
+    private $headers;
+
+    public function __construct(int $statusCode, string $message = null, \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    {
+        $this->statusCode = $statusCode;
+        $this->headers = $headers;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Set response headers
+     *
+     * @param array $headers Response headers
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = $headers;
+    }
+}
---
+++ src/Http/Exception/HttpExceptionInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Twitf\Framework\Http\Exception;
+
+interface HttpExceptionInterface extends \Throwable
+{
+    /**
+     * Returns the status code.
+     *
+     * @return int An HTTP response status code
+     */
+    public function getStatusCode();
+
+    /**
+     * Returns response headers.
+     *
+     * @return array Response headers
+     */
+    public function getHeaders();
+}
